5f693da530701c28e36f2782e91c8543f65ad1c0
arthur mesquita <arthurmesquitag@gmail.com>
2026-04-07 20:33:50 -0300

    JAVASCRIPT
    JAVA

---
+++ aula01/aula02.js
@@ -0,0 +1,13 @@
+let nome = "arthur";
+console.log(nome);
+
+const idae = 30;
+console.log(idade);
+
+let aspasSimples =`estamos criando uma string com aspas simples`
+// string em resumo são texto que podem ser utilizados no javascript
+// e para utiliza-los podemos usar com aspas simples, duplas, ou string template.
+
+let aspasDupla = "string com aspas duplas"
+// exemplo de string com aspas dupla.
+//console.log responsavel por imprimir na tela os resultados
